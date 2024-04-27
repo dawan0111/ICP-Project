b1e7fe19db36208d52c98a85ba286f1ddbe34da3
KDW <msd030428@gmail.com>
2024-04-27 23:34:50 +0900

    add generateRandomPoint util function

--- module/icp/include/icp/utils.hpp
+++ module/icp/include/icp/utils.hpp
@@ -1,11 +1,15 @@
 #ifndef ICP_UTILS_HPP_
 #define ICP_UTILS_HPP_
-
+#define RANDOM_SEED 3244
+#include "icp.hpp"
 #include <Eigen/Core>
 #include <iostream>
+#include <random>
 
 namespace ICP::utils {
-void generateRandomPoints(int16_t length);
-}
+static std::mt19937_64 rng1(RANDOM_SEED);
+ICP::Points generateRandomPoints(int16_t length, const double min = 1.0,
+                                 const double max = 4.0);
+} // namespace ICP::utils
 
 #endif
--- module/icp/src/utils.cpp
+++ module/icp/src/utils.cpp
@@ -1,7 +1,17 @@
 #include "icp/utils.hpp"
 
 namespace ICP::utils {
-void generateRandomPoints(int16_t length) {
-  std::cout << "generateRandomPoint: " << length << std::endl;
+ICP::Points generateRandomPoints(const int16_t length, const double min,
+                                 const double max) {
+  std::uniform_real_distribution dist1(min, max);
+  ICP::Points randomPoints;
+
+  randomPoints.reserve(length);
+
+  for (int16_t i = 0; i < length; i++) {
+    randomPoints.emplace_back(dist1(rng1), dist1(rng1));
+  }
+
+  return randomPoints;
 }
 } // namespace ICP::utils
