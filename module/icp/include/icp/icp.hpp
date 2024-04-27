d88541006d4ac061904a98ce0c5d603a8f05adc2
KDW <msd030428@gmail.com>
2024-04-27 23:09:23 +0900

    update icp module

--- module/icp/include/icp/icp.hpp
+++ module/icp/include/icp/icp.hpp
@@ -1,9 +1,19 @@
 #ifndef ICP_HPP_
 #define ICP_HPP_
+#include <Eigen/Core>
 #include <iostream>
+#include <memory>
 
 class ICP {
 public:
-    ICP();
+  using Points = std::vector<Eigen::Vector3d>;
+  using InputPointsPtr = std::shared_ptr<Points>;
+  ICP();
+  void setInputSource(const InputPointsPtr points);
+  void setInputTarget(const InputPointsPtr points);
+
+private:
+  InputPointsPtr source_;
+  InputPointsPtr target_;
 };
 #endif
--- module/icp/src/icp.cpp
+++ module/icp/src/icp.cpp
@@ -1,5 +1,5 @@
 #include "icp/icp.hpp"
 
-ICP::ICP() {
-    std::cout << "ICP Method!!" << std::endl;
-}
+ICP::ICP() { std::cout << "ICP Method!!" << std::endl; }
+void ICP::setInputSource(InputPointsPtr source) { source_ = source; }
+void ICP::setInputTarget(InputPointsPtr target) { target_ = target; }
